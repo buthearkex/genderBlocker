c86003809a9618f03d03f7f2d7c00a1d2fcd6c90
Butterkex <mikkosh@hotmail.com>
2016-02-05 21:50:36 +0100

    let the magic start

---
+++ myscript.js
@@ -0,0 +1,10 @@
+$(".profile-picture").hide();
+
+/*var name = $(".full-name").text();
+var first = name.slice(0, name.indexOf(" "));
+console.log(first);
+var second = name.slice(1, name.indexOf(" "));
+console.log(second);
+var shortened = first.charAt(0);
+
+$(".full-name").html(shortened + "." + second);*/
