9af39525aa1a043d7847a14db7c566a8d7bcb177
Butterkex <mikkosh@hotmail.com>
2016-02-05 22:11:14 +0100

    hide first name

--- myscript.js
+++ myscript.js
@@ -1,5 +1,23 @@
 $(".profile-picture").hide();
 
+$(".full-name, .name").each(function () {
+    var fullNameStr = $(this).text();
+    var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
+    var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
+    var updatedNameStr = firstNameStr + " " + lastNameStr;
+    $(this).text(updatedNameStr);
+});
+
+/*$('.name').each(function () {
+    var fullNameStr = $(this).text();
+    var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
+    var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
+    var updatedNameStr = firstNameStr + " " + lastNameStr;
+
+    $(this).children().text(updatedNameStr);
+});*/
+
+
 /*var name = $(".full-name").text();
 var first = name.slice(0, name.indexOf(" "));
 console.log(first);
