01327be477a8288620139e2252881bfac8425215
DanielMeusburger <da.meusburger@gmail.com>
2016-02-06 11:53:06 +0100

    Changed myscript

--- myscript.js
+++ myscript.js
@@ -1,20 +1,12 @@
 $(".profile-picture").hide();
+$('.endorse-v2').hide();
 
 $(".full-name, .connections-name, .title.main-headline").each(function () {
-    var fullNameStr = $(this).text();
-    var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
-    var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
-    var updatedNameStr = firstNameStr + " " + lastNameStr;
-    $(this).text(updatedNameStr);
+	$(this).text(replacedStr = replaceFirstName($(this)));
 });
 
 $('.name').each(function () {
-    var fullNameStr = $(this).text();
-    var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
-    var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
-    var updatedNameStr = firstNameStr + " " + lastNameStr;
-
-    $(this).children().text(updatedNameStr);
+    $(this).children().text(replacedStr = replaceFirstName($(this)));
 });
 
 $(".browse-map-photo").each(function(){
@@ -22,22 +14,23 @@ $(".browse-map-photo").each(function(){
     $(this).hide();
 });
 
+$('.endorse-skills-title').each(function(){
+	var fullNameStr = $(this).text();
+	var startStr = fullNameStr.substr(0,fullNameStr.indexOf(' '));
+	var firstNameStr = fullNameStr.substr(fullNameStr.indexOf(' ')+1).charAt(0) + ".";
+	var remainingStr = fullNameStr.substr(fullNameStr.indexOf(' ')+2);
+	var updatedNameStr = startStr + " " + firstNameStr + " " + remainingStr;
+	console.log(updatedNameStr);
+	$(this).children().text(updatedNameStr);
+});
+
 window.setInterval(function () {
-    $('a[href$="name"]').each(function () {
-        var fullNameStr = $(this).text();
-        var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
-        var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
-        var updatedNameStr = firstNameStr + " " + lastNameStr;
-        $(this).text(updatedNameStr);
+    $('a[href$="name"], a[href^="http"][href$="people_cluster"]').each(function () {
+		$(this).text(replacedStr = replaceFirstName($(this)));
     });
 
     $('.new-miniprofile-container').each(function () {
-        var fullNameStr = $(this).text();
-        var firstNameStr = fullNameStr.substr(0, fullNameStr.indexOf(' ')).charAt(0) + ".";
-        var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ') + 1);
-        var updatedNameStr = firstNameStr + " " + lastNameStr;
-        console.log(updatedNameStr);
-        $(this).children().children().text(updatedNameStr);
+        $(this).children().children().text(replacedStr = replaceFirstName($(this)));
     });
 
     $(function () {
@@ -54,3 +47,12 @@ window.setInterval(function () {
     });
 
 }, 1000);
+
+
+function replaceFirstName(element) {
+	var fullNameStr = element.text();
+	var firstNameStr = fullNameStr.substr(0,fullNameStr.indexOf(' ')).charAt(0) + ".";
+	var lastNameStr = fullNameStr.substr(fullNameStr.indexOf(' ')+1);
+	var updatedNameStr = firstNameStr + " " + lastNameStr;
+	return updatedNameStr;	
+}
