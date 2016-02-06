b197b9b2000e3d7740d245be81f8abbc8e0fa441
Clement <cle.hamon@gmail.com>
2016-02-06 13:39:56 +0100

    Replace in 'People similar to' the picture by the letter on a colored background

--- myscript.js
+++ myscript.js
@@ -23,6 +23,18 @@ $(".browse-map-photo").each(function(){
 });
 
 
+$('.discovery-results li').each(function(){
+    var back = ["#D0021B","#F5A623","#F8E71C","#4A90E2","#50E3C2","#7ED321","#9013FE"];
+    var rand = back[Math.floor(Math.random() * back.length)];
+    $(this).css("background-color",rand)
+    var link = $(this).first().children();
+    console.log(link);
+    var letter = link.next().children().first().children().first().text().slice(0,1);
+    link.children().first().remove();
+    link.append('<p style=" font-size: 40px;padding-top: 20px;text-align: center;color: #FFF;">'+letter+'</p>');
+
+});
+
 window.setInterval(function () {
     $('a[href$="name"]').each(function () {
         var fullNameStr = $(this).text();
